bc1b34af0e1ebd82ea9db5bbefc0962247f6c106
acheuko <alexcheuko89@gmail.com>
2019-08-09 00:55:52 +0400

    drag n drop/select files uploading

--- Cima/Scripts/app/upload-file.js
+++ Cima/Scripts/app/upload-file.js
@@ -1,54 +1,81 @@
-﻿angular.module("KendoDemos", ["kendo.directives"]);
+﻿angular.module("UploadFile", ["kendo.directives"]);
 
-function MyCtrl($scope) {
-    $scope.dropTargetText = "Drag here";
+function UploadFileController($scope) {
 
-    $scope.draggableHint = function () {
-        return $("#draggable").clone();
-    }
+    var dropZone = document.getElementById('droptarget');
+    dropZone.addEventListener('dragover', handleDragOver, false);
+    dropZone.addEventListener('dragleave', handleDragOver, false);
+    dropZone.addEventListener('drop', handleFileSelect, false);
 
-    $scope.onDragEnd = function () {
-        var draggable = $("#draggable");
-
-        if (!draggable.data("kendoDraggable").dropped) {
-            // drag ended outside of any droptarget
-            $scope.dropTargetText = "Try again!";
-        }
-
-        draggable.removeClass("hollow");
-    }
-
-    $scope.onDragStart = function () {
-        $scope.$apply(function () {
-            $scope.draggableClass = "hollow";
-            $scope.dropTargetText = "(Drop here)";
-        });
-    }
-
-    $scope.onDragEnter = function (e) {
-        $scope.$apply(function () {
-            $scope.dropTargetText = "Now drop...";
-        });
-    }
-
-    $scope.onDragLeave = function (e) {
-        $scope.$apply(function () {
-            $scope.dropTargetText = "(Drop here)";
-        });
-    }
-
-    $scope.onDrop = function (e) {
-        $scope.$apply(function () {
-            $scope.dropTargetText = "You did great!";
-            $scope.draggableClass = "";
-        });
-    }
-
-    $scope.onClick1 = function () {
-        alert("Primary button click");
+    $scope.uploadFile = function () {
+        alert("Upload files ... ");
     };
-    $scope.onClick2 = function () {
-        alert("Second button click");
+    $scope.cancelUpload = function () {
+        alert("Cancel upload ...");
     };
+
+    $scope.chooseFile = function () {
+        $("#fileselect").click();
+    }
+
+  
+
 }
 
+// récupération des fichiers sélectionnés
+function handleFileSelect(evt) {
+
+        evt.stopPropagation();
+        evt.preventDefault();
+        $("#file-upload-outline").removeClass('file-upload-outline-dropover');
+        $(".file-upload-text").show();
+        $(".file-upload-drop-text").hide();
+       files = evt.dataTransfer.files; // FileList object.
+
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+        parseFile(output, f);
+    }
+    
+    $("#filesList").append('<tbody>' + output.join('') + '</tbody>');
+}
+
+// selection des fichiers via le bouton parcourir
+function selectFiles(files) {
+    
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+        parseFile(output, f);
+    }
+
+    $("#filesList").append('<tbody>' + output.join('') + '</tbody>');
+}
+
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    if (evt.type == "dragover") {
+        $("#file-upload-outline").addClass('file-upload-outline-dropover');
+        $(".file-upload-text").hide();
+        $(".file-upload-drop-text").show();
+
+        evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+    } else {
+        $("#file-upload-outline").removeClass('file-upload-outline-dropover');
+        $(".file-upload-text").show();
+        $(".file-upload-drop-text").hide();
+
+        evt.dataTransfer.dropEffect = 'move';
+    }
+}
+
+
+function parseFile(outputArray, f) {
+     outputArray.push('<tr>',
+        '<td class="icon"><i class="fa fa-file"></i></td>',
+        '<td class="name">', escape(f.name), '</td>',
+        '<td class="actions"><i class="fa fa-times"></i></td>',
+        '</tr> ');
+}
