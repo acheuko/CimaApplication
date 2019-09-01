d08a8210be794b2ce62d52b18c638e36e6df05cb
acheuko <alexcheuko89@gmail.com>
2019-09-02 01:09:45 +0400

    <dev> implement link to execute ssrs report

---
+++ Cima/Controllers/ReportController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace Cima.Controllers
+{
+    public class ReportController : Controller
+    {
+        //
+        // GET: /Report/
+
+        public ActionResult Report()
+        {
+            string reportFolder = ConfigurationManager.AppSettings["ReportPath"].ToString();
+
+            ReportViewer rptViewer = new ReportViewer
+            {
+
+                // ProcessingMode will be Either Remote or Local  
+                ProcessingMode = ProcessingMode.Remote,
+                SizeToReportContent = true,
+                ZoomMode = ZoomMode.PageWidth,
+                Width = Unit.Percentage(100),
+                Height = Unit.Percentage(100),
+                AsyncRendering = true
+            };
+            rptViewer.ServerReport.ReportServerUrl = new Uri("http://localhost/ReportServer/");
+
+            rptViewer.ServerReport.ReportPath = reportFolder;
+                //String.Format("{0}/{1}",reportFolder,"Report1.rdl");
+                //this.SetReportPath();
+
+            ViewBag.ReportViewer = rptViewer;
+            return View();
+        }
+
+
+    }
+}
